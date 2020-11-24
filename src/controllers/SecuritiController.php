c1fba3cac50789f67fe2333712d7bb631f443268
FilipK-PK <ftsk.hj@wp.pl>
2020-11-24 10:45:33 +0100

    Add 4

--- src/controllers/SecuritiController.php
+++ src/controllers/SecuritiController.php
@@ -9,10 +9,10 @@ class SecuritiControler extends AppController
     {
         $user = new Users('abc', '123', 'jan', 'kos');
 
-        if($this->isPost())
+        /*if($this->isPost()) // blad ?
         {
-            return $this->login('logowanie');
-        }
+            return $this->render('logowanie');
+        }*/
 
         $email = $_POST["email"];
         $password = $_POST["password"];
@@ -27,9 +27,9 @@ class SecuritiControler extends AppController
             return $this->render('logowanie', ['messages' => ['Wrong password']]);
         }
 
-       return $this->render('panel_startowy');
+       //return $this->render('panel_startowy');
 
-       // $url = "http://$_SERVER[HTTP_HOST]";
-       // header("Location: {$url}/projects");
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/projects");
     }
 }
